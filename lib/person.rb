51762b92dc289274d6a957ab3220d95bf5cbc6c3
Elise <elise041294@gmail.com>
2022-05-18 11:52:08 -0600

    commit tree ca866a6e9053c60876a69db767717ac792e8a2a7

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,35 @@
 # your code goes here
+class Person
+    attr_reader :name, :happiness 
+    attr_accessor :bank_account
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8 
+        @hygiene = 8 
+    end
+    # taken care by attr_accessor
+    # def bank_account= (value)
+    #     @bank_account = value
+    # end
+    # def bank_account
+    #     @bank_account
+    # end
+
+    #taken care by attr_reader
+    # def happiness
+    #     @happiness
+    # end
+
+    def happiness=(value)
+        if value.between?(0, 10)
+        @happiness = value 
+        elsif value >10
+            @happiness = 10
+        else value < 0
+            @happiness = 0
+        end
+    end
+end
+
